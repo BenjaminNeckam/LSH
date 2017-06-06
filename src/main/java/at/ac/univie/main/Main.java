2d5acecabc5e60c5ec7ec7d00dedd64b9e266c9f
Benjamin <b.neckam@hotmail.com>
2017-06-06 19:50:40 +0200

    Finished Project

--- src/main/java/at/ac/univie/main/Main.java
+++ src/main/java/at/ac/univie/main/Main.java
@@ -1,6 +1,8 @@
 package at.ac.univie.main;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import org.jfree.ui.RefineryUtilities;
 
@@ -12,11 +14,17 @@ import at.ac.unive.utils.Point;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Point> points = CSVParser.CSVToPoint("LSH-nmi-corrected.csv");
-		ArrayList<Integer> compare = CSVParser.CSVToPointCompare("LSH-nmi-compare.csv");
+		
+
+		ArrayList<Point> points;
+		ArrayList<Integer> compare;
+		
 		
 		if(args[0].equals("1")){
 			try{
+				points = CSVParser.CSVToPoint(args[1]);
+				compare = CSVParser.CSVToPointCompare(args[2]);
+				
 				KMeans.lloyd(points,15);
 				ArrayList<Integer> result = KMeans.PointsToIntegerList(points);
 				KMeans.NMI(compare, result);
@@ -26,32 +34,42 @@ public class Main {
 				RefineryUtilities.centerFrameOnScreen(scatterplotdemo4);
 				scatterplotdemo4.setVisible(true);
 			}catch (Exception e) {
+				e.printStackTrace();
 				 System.err.println("An exception was thrown (most of the time for an unknow reason which we couldn't fix)");
 
 			}
 		}else if(args[0].equals("2")){
 			try{
-				Float bucketSize;
-				if(args[1].isEmpty()){
+				Float bucketSize = null;
+				if(args.length == 3){
 					bucketSize = null;
+					points = CSVParser.CSVToPoint(args[1]);
+					compare = CSVParser.CSVToPointCompare(args[2]);
 				}else{
-					bucketSize = Float.valueOf(args[1]);
+					bucketSize = Float.valueOf(args[3]);
+					points = CSVParser.CSVToPoint(args[1]);
+					compare = CSVParser.CSVToPointCompare(args[2]);
 				}
 				KMeans.lsh(points, bucketSize, (float)7.0);
 				ArrayList<Integer> result2 = KMeans.PointsToIntegerList(points);
 				KMeans.NMI(compare, result2);
 			}catch (Exception e) {
+				e.printStackTrace();
 				 System.err.println("An exception was thrown (most of the time for an unknow reason which we couldn't fix)");
 
 			}
 			
 		}else if(args[0].equals("3")){
 			try{
-				Float bucketSize;
-				if(args[1].isEmpty()){
+				Float bucketSize = null;
+				if(args.length == 3){
 					bucketSize = null;
+					points = CSVParser.CSVToPoint(args[1]);
+					compare = CSVParser.CSVToPointCompare(args[2]);
 				}else{
-					bucketSize = Float.valueOf(args[1]);
+					bucketSize = Float.valueOf(args[3]);
+					points = CSVParser.CSVToPoint(args[1]);
+					compare = CSVParser.CSVToPointCompare(args[2]);
 				}
 				KMeans.lloyd(points,15);
 				ArrayList<Integer> result = KMeans.PointsToIntegerList(points);
